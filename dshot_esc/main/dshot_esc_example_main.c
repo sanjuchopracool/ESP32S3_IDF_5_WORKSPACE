7792f4ed01784844661979fb92b25f0136ac40e9
Sanjay <sanjuchopracool@gmail.com>
2022-10-24 17:43:09 +0530

    added initial files

---
+++ dshot_esc/main/dshot_esc_example_main.c
@@ -0,0 +1,94 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/rmt_tx.h"
+#include "dshot_esc_encoder.h"
+
+#define DSHOT_ESC_RESOLUTION_HZ 20000000 // 40MHz resolution, DSHot protocol needs a relative high resolution
+
+#define NUM_CHANNELS			4
+
+static const char *TAG = "example";
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Create RMT TX channel");
+    rmt_channel_handle_t esc_channels[NUM_CHANNELS] = {NULL};
+    int gpio_nums[NUM_CHANNELS] = {1, 2, 42, 41};
+    rmt_tx_channel_config_t tx_chan_config = {
+        .clk_src = RMT_CLK_SRC_DEFAULT, // select a clock that can provide needed resolution
+        .mem_block_symbols = 48,
+        .resolution_hz = DSHOT_ESC_RESOLUTION_HZ,
+        .trans_queue_depth = 2, // set the number of transactions that can be pending in the background
+    };
+
+    for (int i = 0 ; i < NUM_CHANNELS; ++i)
+    {
+    	tx_chan_config.gpio_num = gpio_nums[i];
+    	ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_chan_config, &esc_channels[i]));
+    }
+
+
+    ESP_LOGI(TAG, "Install Dshot ESC encoder");
+    rmt_encoder_handle_t dshot_encoders[NUM_CHANNELS] = {NULL};
+    dshot_esc_encoder_config_t encoder_config = {
+        .resolution = DSHOT_ESC_RESOLUTION_HZ,
+        .baud_rate = 300000, // DSHOT150 protocol
+        .post_delay_us = 50, // extra delay between each frame
+    };
+
+    for (int i = 0 ; i < NUM_CHANNELS; ++i) {
+    	ESP_ERROR_CHECK(rmt_new_dshot_esc_encoder(&encoder_config, &dshot_encoders[i]));
+    }
+
+    ESP_LOGI(TAG, "Enable RMT TX channel");
+    for (int i = 0 ; i < NUM_CHANNELS; ++i) {
+    	ESP_ERROR_CHECK(rmt_enable(esc_channels[i]));
+    }
+
+//    // install sync manager
+//    rmt_sync_manager_handle_t synchro = NULL;
+//    rmt_sync_manager_config_t synchro_config = {
+//        .tx_channel_array = esc_channels,
+//        .array_size = sizeof(esc_channels) / sizeof(esc_channels[0]),
+//    };
+//    ESP_ERROR_CHECK(rmt_new_sync_manager(&synchro_config, &synchro));
+
+
+    rmt_transmit_config_t tx_config = {
+        .loop_count = -1, // infinite loop
+    };
+    dshot_esc_throttle_t throttle = {
+        .throttle = 0,
+        .telemetry_req = false, // telemetry is not supported in this example
+    };
+
+    ESP_LOGI(TAG, "Start ESC by sending zero throttle for a while...");
+    for (int i = 0 ; i < NUM_CHANNELS; ++i) {
+    	ESP_ERROR_CHECK(rmt_transmit(esc_channels[i], dshot_encoders[i], &throttle, sizeof(throttle), &tx_config));
+    }
+    vTaskDelay(pdMS_TO_TICKS(5000));
+
+    ESP_LOGI(TAG, "Increase throttle, no telemetry");
+    for (uint16_t thro = 100; thro <= 500; thro += 10) {
+        throttle.throttle = thro;
+
+        for (int i = 0 ; i < NUM_CHANNELS; ++i) {
+            ESP_ERROR_CHECK(rmt_transmit(esc_channels[i], dshot_encoders[i], &throttle, sizeof(throttle), &tx_config));
+            // the previous loop transfer is till undergoing, we need to stop it and restart,
+            // so that the new throttle can be updated on the output
+            ESP_ERROR_CHECK(rmt_disable(esc_channels[i]));
+            ESP_ERROR_CHECK(rmt_enable(esc_channels[i]));
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
+        if (thro >= 500)
+        	thro = 100;
+    }
+
+}
